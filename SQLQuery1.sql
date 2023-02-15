c9a83d6e2b20de217262888c2c20307884c16b62
konate-souleymane <115789198+konate-souleymane@users.noreply.github.com>
2023-02-15 11:54:20 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+
+--CREATE DATABASE ODS
+--CREATE DATABASE DWH
+
+
+CREATE TABLE ODS.dbo.BNLC(
+
+id_lieu varchar(max),
+id_local varchar(max),
+nom_lieu varchar(max),
+ad_lieu varchar(max),
+insee varchar(max),
+typee varchar(max),
+date_maj varchar(max),
+ouver varchar(max),
+sourc varchar(max),
+xlong varchar(max),
+ylat varchar(max),
+nbre_pl varchar(max),
+nbre_pmr varchar(max),
+durer varchar(max),
+horaire varchar(max),
+proprio varchar(max),
+lumiere varchar(max),
+comm varchar(max)
+
+)
+
+
+
